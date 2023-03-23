f01b733ca9ea9a93f31751dcb602d2aee030b1db
Sergey Solodovnyk <xxxsergxxx1990@gmail.com>
2023-03-23 20:05:27 +0200

    q

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -14,3 +14,13 @@ export const fetchCountries = (name) => {
 };
 
 
+
+// export const fetchCountries = (name) => {
+//     return fetch(`${BASE_URL}${name}?fields=name,capital,population,flags,languages`)
+//         .then(response => {
+//             if (response.status === 404) {
+//                 throw new Error(response.status);
+//             }
+//             return response.json();
+//         });
+// };
